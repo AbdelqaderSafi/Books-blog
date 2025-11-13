cd6bce9f23424040c71190fcf805ed579f4eae5d
Abdelqader <saabood519@gmail.com>
2025-11-13 13:03:04 +0200

    Initial commit: Strapi author site backend

---
+++ config/plugins.ts
@@ -0,0 +1,14 @@
+import { env } from "process";
+
+export default () => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-imagekit",
+      providerOptions: {
+        publicKey: env.IMAGEKIT_PUBLIC_KEY,
+        privateKey: env.IMAGEKIT_PRIVATE_KEY,
+        urlEndpoint: env.IMAGEKIT_URL_ENDPOINT,
+      },
+    },
+  },
+});
---
+++ types/generated/components.d.ts
@@ -0,0 +1,21 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'link';
+    icon: 'arrowRight';
+  };
+  attributes: {
+    Label: Schema.Attribute.String;
+    URL: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.link': SharedLink;
+    }
+  }
+}
